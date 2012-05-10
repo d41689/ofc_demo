f8991621d6935c563e09e702e3dd07e94738e7a1
dyj <d41689@gmail.com>
2012-05-10 18:53:55 +0800

    add code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OfcDemo::Application.config.secret_token = '1d325e77212ef7bb562dff0c5b2a280b320905701f06ab3a1491f1bcf7373df92e73103e27267750a9f548bca363f2a42b0f0c57790083013283b75f4aaf1d2c'
